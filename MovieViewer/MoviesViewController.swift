624b842b1d53d073f2894cbaf6b66989ef50cc3f
Erica Lee <Erica@Ericas-MacBook-Pro-3.local>
2016-01-21 22:32:57 -0600

    TableView set up

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        TableView.dataSource = self
+        TableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 20 // will have 20 rows
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        cell.textLabel!.text = "row \(indexPath.row)"
+        print("row\(indexPath.row)")
+        return cell
     }
     
 
